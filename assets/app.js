2dad974ed67e811393ef4fbc4cccbbf78a764127
jonathan-banon <j.banon01@gmail.com>
2024-12-04 18:52:01 +0100

    install : vuejs package

--- assets/app.js
+++ assets/app.js
@@ -5,5 +5,9 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import { createApp } from 'vue';
+import App from './js/components/App.vue';
+
+createApp(App).mount('#app');
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
