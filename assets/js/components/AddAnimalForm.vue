10bd44831bda0e30f79aac7bfc115cba273f27e1
jonathan-banon <j.banon01@gmail.com>
2024-12-26 13:38:30 +0100

    fix : LoginPopup + AddAnimalForm on Popup

--- assets/js/components/AddAnimalForm.vue
+++ assets/js/components/AddAnimalForm.vue
@@ -1,62 +1,65 @@
 <template>
-  <form class="flex justify-between w-full" @submit.prevent="addAnimal">
-    <div class="flex justify-between w-3/5">
-      <div class="animal-picture w-2/5 bg-primary rounded-3xl" :style="{
-        backgroundImage: 'url(' + newAnimal.images[0] + ')',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-      }">
-        <div class="overlay bg-opacity-50 p-2">
-          <input type="file" @change="newOnFileChange($event)" multiple>
+  <div class="bg-secondary p-8 rounded-lg shadow-lg w-7/12">
+    <h2 class="text-2xl font-semibold text-center mb-6">Ajouter un animal</h2>
+    <form class="flex flex-col justify-between" @submit.prevent="addAnimal">
+      <div class="flex justify-between">
+        <div class="animal-picture w-2/5 bg-primary rounded-3xl" :style="{
+          backgroundImage: 'url(' + newAnimal.images[0] + ')',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }">
+          <div class="overlay bg-opacity-50 p-2">
+            <input type="file" @change="newOnFileChange($event)" multiple>
+          </div>
         </div>
-      </div>
-      <div>
-        <div>
-          <label for="name">Nom</label>
-          <input required v-model="newAnimal.name" type="text" id="name" class="animal-input" />
-        </div>
-        <label for="type">Type</label>
-        <select required v-model="newAnimal.type" id="type" class="animal-input" @change="fetchBreeds(newAnimal.type)">
-          <option value="" disabled>Sélectionnez un type</option>
-          <option v-for="type in types" :key="type.id" :value="type.id">
-            {{ type.name }}
-          </option>
-        </select>
-
-        <div v-if="isTypeSelected">
-          <label for="breed">Race</label>
-          <select required v-model="newAnimal.breed" id="breed" class="animal-input">
-            <option value="" disabled>Sélectionnez une race</option>
-            <option v-for="breed in breeds" :key="breed.id" :value="breed.id">
-              {{ breed.name }}
+        <div class="w-5/12">
+          <div>
+            <label for="name">Nom</label>
+            <input required v-model="newAnimal.name" type="text" id="name" class="animal-input" />
+          </div>
+          <label for="type">Type</label>
+          <select required v-model="newAnimal.type" id="type" class="animal-input" @change="fetchBreeds(newAnimal.type)">
+            <option value="" disabled>Sélectionnez un type</option>
+            <option v-for="type in types" :key="type.id" :value="type.id">
+              {{ type.name }}
             </option>
           </select>
-        </div>
-
-        <div>
-          <label for="age">Âge</label>
-          <input required v-model="newAnimal.age" type="number" min="1" id="age" class="animal-input" />
-        </div>
-
-        <div>
-          <label for="description">Description</label>
-          <textarea required v-model="newAnimal.description" id="description" class="animal-input"></textarea>
-        </div>
-
-        <div>
-          <label for="price">Prix</label>
-          <input required v-model="newAnimal.price" type="number" min="0" id="price" class="animal-input" />
+  
+          <div v-if="isTypeSelected">
+            <label for="breed">Race</label>
+            <select required v-model="newAnimal.breed" id="breed" class="animal-input">
+              <option value="" disabled>Sélectionnez une race</option>
+              <option v-for="breed in breeds" :key="breed.id" :value="breed.id">
+                {{ breed.name }}
+              </option>
+            </select>
+          </div>
+  
+          <div>
+            <label for="age">Âge</label>
+            <input required v-model="newAnimal.age" type="number" min="1" id="age" class="animal-input" />
+          </div>
+  
+          <div>
+            <label for="description">Description</label>
+            <textarea required v-model="newAnimal.description" id="description" class="animal-input"></textarea>
+          </div>
+  
+          <div>
+            <label for="price">Prix</label>
+            <input required v-model="newAnimal.price" type="number" min="0" id="price" class="animal-input" />
+          </div>
         </div>
       </div>
-    </div>
-
-    <div class="flex flex-col justify-between items-end">
-      <button class="p-4 bg-primary font-semibold rounded-md focus:outline-none" @click="$emit('close')">Retour</button>
-      <button type="submit" class="p-4 bg-primary font-semibold rounded-md focus:outline-none">
-        Ajouter l'animal
-      </button>
-    </div>
-  </form>
+  
+      <div class="flex justify-between items-end mt-5">
+        <button class="p-4 bg-primary font-semibold rounded-md focus:outline-none" @click="$emit('close')">Retour</button>
+        <button type="submit" class="p-4 bg-primary font-semibold rounded-md focus:outline-none">
+          Ajouter l'animal
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 <script>
 export default {
--- assets/js/components/App.vue
+++ assets/js/components/App.vue
@@ -1,32 +1,31 @@
 <template>
     <div id="app">
-        <template v-if="isPopupVisible">
-            <Popup :popup="popup" @close="closeLoginPopup"/>
-        </template>
+        <div v-if="isPopupVisible"
+            class="flex justify-center items-center min-h-screen w-full h-full z-40 fixed bg-black">
+            <LoginPopup :popup="popup" @close="closeLoginPopup" />
+            <template v-if="showAddForm">
+                <AddAnimalForm :types="types" :breeds="breeds" @close="toggleAddForm" @addAnimal="addAnimal"
+                    @fetchBreeds="fetchBreeds" />
+            </template>
+            <template v-if="showTypeForm">
+                <AddTypeForm @close="toggleTypeForm" @addType="addType" />
+            </template>
+            <template v-if="showBreedForm">
+                <BreedForm :types="types" :breeds="breeds" :trashUrl="trashUrl" :penUrl="penUrl"
+                    @toggle-breed-form="toggleBreedForm" @add-breed="addBreed" @fetch-breeds="fetchBreeds"
+                    @del-breed="delBreed" @edit-breed="editBreed" @save-edit-breed="saveEditBreed"
+                    @cancel-edit-breed="cancelEditBreed" />
+            </template>
+        </div>
         <Navbar :types="types" :isAlertVisible="isAlertVisible" :alertMessage="alertMessage" :urlLogo="urlLogo"
             :trashUrl="trashUrl" :penUrl="penUrl" :isUserLoggedIn="isUserLoggedIn" @toggleLogin="toggleLoginPopup"
-            @fetchAnimals="fetchAnimals" @saveEditType="saveEditType" @delType="delType"
-            :actualTypeId="actualTypeId" />
+            @fetchAnimals="fetchAnimals" @saveEditType="saveEditType" @delType="delType" :actualTypeId="actualTypeId" />
     </div>
     <template v-if="isUserLoggedIn">
         <div class="home-container flex justify-end p-5">
             <AdminNav :showAddForm="showAddForm" :showTypeForm="showTypeForm" :showBreedForm="showBreedForm"
                 @toggleAddForm="toggleAddForm" @toggleTypeForm="toggleTypeForm" @toggleBreedForm="toggleBreedForm" />
             <div class="w-3/4">
-                <template v-if="showAddForm">
-                    <AddAnimalForm :types="types" :breeds="breeds" @close="toggleAddForm" @addAnimal="addAnimal"
-                        @fetchBreeds="fetchBreeds" />
-                </template>
-                <div class="test"></div>
-                <template v-if="showTypeForm">
-                    <AddTypeForm @close="toggleTypeForm" @addType="addType" />
-                </template>
-                <template v-if="showBreedForm">
-                    <BreedForm :types="types" :breeds="breeds" :trashUrl="trashUrl" :penUrl="penUrl"
-                        @toggle-breed-form="toggleBreedForm" @add-breed="addBreed" @fetch-breeds="fetchBreeds"
-                        @del-breed="delBreed" @edit-breed="editBreed" @save-edit-breed="saveEditBreed"
-                        @cancel-edit-breed="cancelEditBreed" />
-                </template>
                 <AdminAnimals :animals="animals" :breeds="breeds" :trashUrl="trashUrl" @edit-animal="editAnimal"
                     @del-image="delImage" @prev-image="prevImage" @next-image="nextImage" @on-file-change="onFileChange"
                     @upload-image="uploadImage" @del-animal="delAnimal" />
@@ -49,7 +48,7 @@ import AddTypeForm from './AddTypeForm.vue';
 import BreedForm from './BreedForm.vue';
 import AdminAnimals from './AdminAnimals.vue';
 import UserAnimals from './UserAnimals.vue';
-import Popup from './Popup.vue';
+import LoginPopup from './LoginPopup.vue';
 
 export default {
     components: {
@@ -60,7 +59,7 @@ export default {
         BreedForm,
         AdminAnimals,
         UserAnimals,
-        Popup
+        LoginPopup
     },
     data() {
         return {
@@ -212,6 +211,7 @@ export default {
             if (response.ok) {
                 this.fetchAnimals(this.actualTypeId)
                 this.showAddForm = false;
+                this.isPopupVisible = false;
                 this.isAlertVisible = true;
                 this.alertMessage = "Animal ajouté avec succès";
                 setTimeout(() => {
@@ -311,6 +311,7 @@ export default {
         },
         toggleAddForm() {
             this.showAddForm = !this.showAddForm
+            this.isPopupVisible = !this.isPopupVisible
         },
         toggleTypeForm() {
             this.showTypeForm = !this.showTypeForm
