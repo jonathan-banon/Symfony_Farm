185df761c4572ab9cd0990b339e43a3421e171c5
jonathan-banon <j.banon01@gmail.com>
2025-02-10 13:07:41 +0100

    fix : url on lginForm

--- assets/js/components/LoginForm.vue
+++ assets/js/components/LoginForm.vue
@@ -66,7 +66,7 @@ export default {
             }
 
             try {
-                const response = await fetch('http://127.0.0.1:8000/login', {
+                const response = await fetch('/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
